2f2ff4cbd6a7252e7f25d284e801b1e77028b629
Khaled Alsaleh <khalsalehsyria1@gmail.com>
2021-05-12 04:56:31 +0200

    Delete Not Used Variables & Clean Up

--- backend/scraping.js
+++ backend/scraping.js
@@ -1,17 +1,9 @@
+import fs from 'fs';
 import axios from 'axios';
 import cheerio from 'cheerio';
-import fs from 'fs';
 
-  let name;
-let image;
-let description;
-let status;
-let category;
-let price;
-let countInStock;
-let rating;
-let numReviews;
-  const json = [{
+const bundles = [
+  {
     name: '',
     image: '',
     description: '',
@@ -21,7 +13,8 @@ let numReviews;
     countInStock: 0,
     rating: 0,
     numReviews: 0,
-  }, {
+  },
+  {
     name: '',
     image: '',
     description: '',
@@ -31,7 +24,8 @@ let numReviews;
     countInStock: 0,
     rating: 0,
     numReviews: 0,
-  }, {
+  },
+  {
     name: '',
     image: '',
     description: '',
@@ -41,13 +35,9 @@ let numReviews;
     countInStock: 0,
     rating: 0,
     numReviews: 0,
-  }];
+  },
+];
 
-// const bundlesNames = [];
-// const bundlesImages = [];
-// const bundlesCategories = [];
-// const bundlesDescriptions = [];
-// const writeStream = fs.createWriteStream('test.js');
 // This is a sample website to scrape data from it, we could change it later...
 const prepareBundlesData = () => {
   axios.get('https://shop.mindfulchef.com/').then((res) => {
@@ -56,27 +46,22 @@ const prepareBundlesData = () => {
     $('.grid__item')
       .find('.collection-item__title')
       .each((index, element) => {
-         name = $(element).text();
-         json[index].name = name;
-         console.log(index);
-        // bundlesNames[index] = { name };
-        // writeStream.write(`${bundlesNames}`);
+        bundles[index].name = $(element).text();
       });
 
     $('.grid__item')
       .find('.lazyload-blur-wrapper')
       .each((index, element) => {
-         image = $(element).children('img').attr('src').replace(/150x/g, '1000x1000');
-        if (image.includes('collections')) {
-          json[index].image = image;
+        const link = $(element).children('img').attr('src').replace(/150x/g, '1000x1000');
+        if (link.includes('collections')) {
+          bundles[index].image = link;
 
-           category = image
+          bundles[index].category = link
             .split('/collections/')
             .pop()
             .split('_650x650_')[0]
             .replace(/_/g, ' ')
             .toUpperCase();
-          json[index].category = category;
         }
       });
   });
@@ -86,61 +71,68 @@ const prepareBundlesDescription = () => {
   axios.get('https://shop.mindfulchef.com/collections/healthy-ready-meals').then((res) => {
     const $ = cheerio.load(res.data);
 
-    json[0].description = $('.collection-sidebar__description')
-        .last('p')
-        .last('span')
-        .text()
-        .replace(/\s\s+/g, '')
-        .split('\n')[0];
+    bundles[0].description = $('.collection-sidebar__description')
+      .last('p')
+      .last('span')
+      .text()
+      .replace(/\s\s+/g, '')
+      .split('\n')[0];
 
-      json[0].status = $('.collection-sidebar__description')
-        .last('p')
-        .last('span')
-        .text()
-        .replace(/\s\s+/g, '')
-        .split('\n')[1];
+    bundles[0].status = $('.collection-sidebar__description')
+      .last('p')
+      .last('span')
+      .text()
+      .replace(/\s\s+/g, '')
+      .split('\n')[1];
   });
 
   axios.get('https://shop.mindfulchef.com/collections/smoothies').then((res) => {
     const $ = cheerio.load(res.data);
 
-    json[1].description = $('.collection-sidebar__description')
-        .last('p')
-        .last('span')
-        .text()
-        .replace(/\s\s+/g, '')
-        .split('\n')[0];
+    bundles[1].description = $('.collection-sidebar__description')
+      .last('p')
+      .last('span')
+      .text()
+      .replace(/\s\s+/g, '')
+      .split('\n')[0];
 
-      json[1].status = $('.collection-sidebar__description')
-        .last('p')
-        .last('span')
-        .text()
-        .replace(/\s\s+/g, '')
-        .split('\n')[1];
+    bundles[1].status = $('.collection-sidebar__description')
+      .last('p')
+      .last('span')
+      .text()
+      .replace(/\s\s+/g, '')
+      .split('\n')[1];
   });
 
-  axios.get('https://shop.mindfulchef.com/collections/gift-vouchers').then((res) => {
-    const $ = cheerio.load(res.data);
+  axios
+    .get('https://shop.mindfulchef.com/collections/gift-vouchers')
+    .then((res) => {
+      const $ = cheerio.load(res.data);
 
-    json[2].description = $('.collection-sidebar__description')
+      bundles[2].description = $('.collection-sidebar__description')
         .last('p')
         .last('span')
         .text()
         .replace(/\s\s+/g, '')
         .split('\n')[0];
 
-      json[2].status = $('.collection-sidebar__description')
+      bundles[2].status = $('.collection-sidebar__description')
         .last('p')
         .last('span')
         .text()
         .replace(/\s\s+/g, '')
         .split('\n')[1];
-  })
-  .then(() => {
-      fs.writeFile('./data/bundels.json', JSON.stringify(json, null, 4), (error) => {
-      console.log('success');
+      if (!bundles[2].status) {
+        bundles[2].status = '';
+      }
+    })
+    .then(() => {
+      fs.writeFile('./data/bundles.json', JSON.stringify(bundles, null, 4), (error) => {
+        if (error) {
+          throw new Error(error);
+        }
+      });
     });
-  });
 };
 
 prepareBundlesData();
