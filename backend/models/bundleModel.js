baadeb8ed65584f6108557ed89ec3db54c3db049
Vaida <93vaida@gmail.com>
2021-05-13 14:25:46 +0200

    added updated schemas from Khaleds code

---
+++ backend/models/bundleModel.js
@@ -0,0 +1,98 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const bundleSchema = mongoose.Schema(
+  {
+    createdByUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    ingredient: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Ingredient',
+      },
+    ],
+    // bundleItems: [
+    //   {
+    //     name: { type: String, required: true },
+    //     image: { type: String, required: true },
+    //     price: { type: Number, required: true },
+    //     ingredient: {
+    //       type: mongoose.Schema.Types.ObjectId,
+    //       required: true,
+    //       ref: 'Ingredient',
+    //     },
+    //   },
+    // ],
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      default: ' ',
+    },
+    status: {
+      type: String,
+      required: true,
+      default: ' ',
+    },
+    category: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Bundle = mongoose.model('Bundle', bundleSchema);
+
+export default Bundle;
