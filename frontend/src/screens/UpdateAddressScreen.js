5313dc9512e4738410cf10e618d2e2ef710baf8e
Ahmad Magdy <amagsid@gmail.com>
2021-05-15 17:11:06 +0200

    fixing refresh issue in UpdateAddressScreen

--- frontend/src/screens/UpdateAddressScreen.js
+++ frontend/src/screens/UpdateAddressScreen.js
@@ -3,10 +3,13 @@ import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
 import Message from '../components/Message';
+import Loader from '../components/Loader';
+
 import { updateSubscription } from '../actions/subscriptionActions';
 
 const UpdateAddressScreen = ({ match, history }) => {
   const subscriptionId = match.params.id;
+  const [message, setMessage] = useState(null);
 
   const subscriptionListMy = useSelector((state) => state.subscriptionListMy);
   const {
@@ -16,23 +19,32 @@ const UpdateAddressScreen = ({ match, history }) => {
   } = subscriptionListMy;
 
   const subscriptionUpdate = useSelector((state) => state.subscriptionUpdate);
-  const { loading, subscription: subscriptionState, success, error } = subscriptionUpdate;
+  const { success, subscription: updatedSubscription } = subscriptionUpdate;
 
-  // if (subscriptions) {
+  // console.log(subscriptions.length > 0);
+
   const subscription = subscriptions.find((sub) => {
     return sub._id === subscriptionId;
   });
-  // }
 
-  console.log(subscription);
+  // console.log(loadingSubscriptions);
+  // console.log(errorSubscriptions);
+  // console.log(subscription);
+  // console.log('hey');
+  // console.log(success);
+  // console.log(updatedSubscription);
 
-  // const cart = useSelector((state) => state.cart);
   const { shippingAddress } = subscription;
 
-  const [address, setAddress] = useState(shippingAddress.address);
-  const [city, setCity] = useState(shippingAddress.city);
-  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
-  const [country, setCountry] = useState(shippingAddress.country);
+  // const [address, setAddress] = useState('');
+  // const [city, setCity] = useState('');
+  // const [postalCode, setPostalCode] = useState('');
+  // const [country, setCountry] = useState('');
+
+  const [address, setAddress] = useState(shippingAddress.address || {});
+  const [city, setCity] = useState(shippingAddress.city || {});
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode || {});
+  const [country, setCountry] = useState(shippingAddress.country || {});
 
   const dispatch = useDispatch();
 
@@ -51,59 +63,75 @@ const UpdateAddressScreen = ({ match, history }) => {
     history.push('/profile');
   };
 
-  // console.log(address, city, postalCode, country, subscriptionId);
+  // Object.size = function (obj) {
+  //   var size = 0,
+  //     value;
+  //   for (value in obj) {
+  //     if (obj.hasOwnProperty(value)) size++;
+  //   }
+  //   return size;
+  // };
+
+  // var size = Object.size(shippingAddress);
+
   return (
     <FormContainer>
       <h1>Edit Your Address</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group controlId="address">
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter address"
-            value={address || ''}
-            required
-            onChange={(e) => setAddress(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+      {loadingSubscriptions ? (
+        <Loader />
+      ) : errorSubscriptions ? (
+        <Message variant="danger">{errorSubscriptions}</Message>
+      ) : (
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="address">
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter address"
+              value={address || ''}
+              required
+              onChange={(e) => setAddress(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-        <Form.Group controlId="city">
-          <Form.Label>City</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter city"
-            value={city || ''}
-            required
-            onChange={(e) => setCity(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+          <Form.Group controlId="">
+            <Form.Label>City</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter city"
+              value={city || ''}
+              required
+              onChange={(e) => setCity(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-        <Form.Group controlId="postalCode">
-          <Form.Label>Postal Code</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter postal code"
-            value={postalCode || ''}
-            required
-            onChange={(e) => setPostalCode(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+          <Form.Group controlId="postalCode">
+            <Form.Label>Postal Code</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter postal code"
+              value={postalCode || ''}
+              required
+              onChange={(e) => setPostalCode(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-        <Form.Group controlId="country">
-          <Form.Label>Country</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter country"
-            value={country || ''}
-            required
-            onChange={(e) => setCountry(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+          <Form.Group controlId="country">
+            <Form.Label>Country</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter country"
+              value={country || ''}
+              required
+              onChange={(e) => setCountry(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-        <Button type="submit" variant="primary">
-          Continue
-        </Button>
-      </Form>
+          <Button type="submit" variant="primary">
+            Continue
+          </Button>
+        </Form>
+      )}
     </FormContainer>
   );
 };
