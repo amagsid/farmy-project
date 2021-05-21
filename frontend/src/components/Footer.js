85ec4c95685c3135ce9300afeab6873a760ccad1
saeedrafay <saeedahmadrafay@hotmail.com>
2021-05-22 00:36:42 +0200

    All page views tracking

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
+import ReactGA from 'react-ga';
+import env from 'react-dotenv';
 
 const Footer = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.initialize(env.GUA_ID);
+    let pagePath = location.search ? location.pathname + location.search : location.pathname;
+    ReactGA.pageview(`${pagePath}`);
+  }, [location]);
+
   return (
     <footer>
       <Container>
