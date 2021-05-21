6e1dfce212d8246486accb72839b0d3f7b49b390
Ehab Wasel <ehab.wasel@yahoo.com>
2021-05-21 21:52:22 +0200

    Implement Google And FB for Register Screen

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios'
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
@@ -8,114 +9,149 @@ import FormContainer from '../components/FormContainer';
 import { register } from '../actions/userActions';
 import Email from 'react-email-autocomplete';
 import emailValidator from 'validator';
+import GoogleAuth from '../components/GoogleAuth';
+import FacebookAuth from '../components/FacebookAuth';
+import useEventGaTracker from '../hooks/useEventGaTracker';
 
 const RegisterScreen = ({ location, history }) => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [message, setMessage] = useState(null);
-  const [isValidEmailMessage, setIsValidEmailMessage] = useState(null);
-  const [isNotValidEmailMessage, setIsNotValidEmailMessage] = useState(null);
-  const validatedEmail = emailValidator.isEmail(email.trim());
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [message, setMessage] = useState(null)
+  const [apiKey, setApiKey] = useState('')
+  const EventGaTracker = useEventGaTracker('SignUp')
 
-  const validateEmail = () => {
-    if (validatedEmail) {
-      setIsValidEmailMessage('Valid Email.');
-    } else {
-      setIsNotValidEmailMessage('Enter valid Email!');
-    }
-  };
+  const dispatch = useDispatch()
+  const userHistoryRoutes = useSelector((state) => state.userHistoryRoutes)
+  const { routesHistory } = userHistoryRoutes
+  const signupOriginPath = routesHistory[routesHistory.length - 3]
 
-  const dispatch = useDispatch();
+  const userRegister = useSelector((state) => state.userRegister)
+  const { loading, error } = userRegister
 
-  const userRegister = useSelector((state) => state.userRegister);
-  const { loading, error, userInfo } = userRegister;
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
 
-  const redirect = '/register/bundleplan';
+  const redirect = location.search ? location.search.split('=')[1] : '/'
 
   useEffect(() => {
     if (userInfo) {
-      history.push(redirect);
+      history.push(redirect)
+    } else {
+      const getApiKey = async () => {
+        try {
+          const { data } = await axios.get('/api/config/authid')
+          if (data) {
+            setApiKey(data)
+          } else {
+            throw new Error('failed to fetch the api key')
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+
+      getApiKey()
     }
-  }, [history, userInfo, redirect]);
+  }, [history, userInfo, redirect])
 
   const submitHandler = (e) => {
-    e.preventDefault();
-
+    e.preventDefault()
     if (password !== confirmPassword) {
-      setMessage('Passwords do not match');
-    } else if (!validatedEmail) {
-      setMessage('Email is not valid');
+      setMessage('Passwords do not match')
     } else {
-      dispatch(register(name, email, password));
+      dispatch(register(name, email, password))
+      EventGaTracker('successfull signup', signupOriginPath)
     }
-  };
+  }
 
   return (
     <FormContainer>
       <h1>Sign Up</h1>
-      {message && <Message variant="danger">{message}</Message>}
-      {error && <Message variant="danger">{error}</Message>}
+      {message && <Message variant='danger'>{message}</Message>}
+      {error && <Message variant='danger'>{error}</Message>}
       {loading && <Loader />}
       <Form onSubmit={submitHandler}>
-        <Form.Group controlId="name">
+        <Form.Group controlId='name'>
           <Form.Label>Name</Form.Label>
           <Form.Control
-            type="name"
-            placeholder="Enter name"
+            type='name'
+            placeholder='Enter name'
             value={name}
             onChange={(e) => setName(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId="email" onBlur={validateEmail}>
+        <Form.Group controlId='email'>
           <Form.Label>Email Address</Form.Label>
-          <Email
-            className="form-control"
-            type="email"
-            placeholder="Enter email"
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-          />
+          ></Form.Control>
         </Form.Group>
-        {isValidEmailMessage ? (
-          <Message variant="info">{isValidEmailMessage}</Message>
-        ) : isNotValidEmailMessage ? (
-          <Message variant="danger">{isNotValidEmailMessage}</Message>
-        ) : null}
 
-        <Form.Group controlId="password">
+        <Form.Group controlId='password'>
           <Form.Label>Password</Form.Label>
           <Form.Control
-            type="password"
-            placeholder="Enter password"
+            type='password'
+            placeholder='Enter password'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        <Form.Group controlId="confirmPassword">
+
+        <Form.Group controlId='confirmPassword'>
           <Form.Label>Confirm Password</Form.Label>
           <Form.Control
-            type="password"
-            placeholder="Confirm password"
+            type='password'
+            placeholder='Confirm password'
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        <Button type="submit" variant="primary">
+
+        <Button type='submit' variant='success' className='mt-2 btn btn-block'>
           Register
         </Button>
       </Form>
 
-      <Row className="py-3">
+      <div className='or'> OR </div>
+      {apiKey && (
+        <>
+          <GoogleAuth
+            apiKey={apiKey.googleid}
+            registerEvent={() => {
+              EventGaTracker(
+                'successfull signup with google ',
+                signupOriginPath
+              )
+            }}
+          />
+          <FacebookAuth
+            apiKey={apiKey.facebookid}
+            registerEvent={() => {
+              EventGaTracker(
+                'successfull signup with facebook ',
+                signupOriginPath
+              )
+            }}
+          />
+        </>
+      )}
+
+      <Row className='py-3'>
         <Col>
-          Have an Account?
-          <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>Login</Link>
+          Have an Account?{' '}
+          <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+            Login
+          </Link>
         </Col>
       </Row>
     </FormContainer>
-  );
-};
+  )
+}
 
 export default RegisterScreen;
