01fd49fe66f1eeef550e428ccb7a4bfc223f5eb2
Vaida <93vaida@gmail.com>
2021-05-21 16:13:50 +0200

    put pointers and pop up in map with fake data

--- frontend/src/components/FarmsMap.js
+++ frontend/src/components/FarmsMap.js
@@ -1,86 +1,91 @@
-// import React, { useRef, useEffect, useState } from 'react';
-// import mapboxgl from 'mapbox-gl';
-// import '../index.css';
-
-// mapboxgl.accessToken =
-//   'pk.eyJ1IjoiZmFybXkiLCJhIjoiY2tveGQ1OGs4MGV4bzJucGRoZ2VlNDVqbSJ9.Jv0nA83huBK0OEc6cmZaTg';
-
-// const FarmsMap = () => {
-//   const mapContainerRef = useRef(null);
-
-//   const [lng, setLng] = useState(-87.65);
-//   const [lat, setLat] = useState(41.84);
-//   const [zoom, setZoom] = useState(10);
-
-//   const data = [
-//     {
-//       coords: ['-87.637596', '41.940403'],
-//     },
-//     {
-//       coords: ['-87.637596', '41.940403'],
-//     },
-//   ];
-//   // Initialize map when component mounts
-//   useEffect(() => {
-//     const map = new mapboxgl.Map({
-//       container: mapContainerRef.current,
-//       style: 'mapbox://styles/mapbox/streets-v11',
-//       center: [lng, lat],
-//       zoom: zoom,
-//     });
-
-//     // Create default markers
-//     data.map((feature) => new mapboxgl.Marker().setLngLat(feature.coords).addTo(map));
-
-//     // Add navigation control (the +/- zoom buttons)
-//     map.addControl(new mapboxgl.NavigationControl(), 'top-right');
-
-//     map.on('move', () => {
-//       setLng(map.getCenter().lng.toFixed(4));
-//       setLat(map.getCenter().lat.toFixed(4));
-//       setZoom(map.getZoom().toFixed(2));
-//     });
-
-//     // Clean up on unmount
-//     return () => map.remove();
-//   }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-//   return (
-//     <div>
-//       <div>
-//         <div>
-//           Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-//         </div>
-//       </div>
-//       <div ref={mapContainerRef} />
-//     </div>
-//   );
-// };
-
-// export default FarmsMap;
-import React from "react";
-import ReactMapGL, { Marker } from 'react-map-gl';
+import React, { useState } from 'react';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const FarmsMap = () => {
-  // const Map = ReactMapboxGl({
-  //   accessToken:
-  //     'pk.eyJ1IjoiZmFybXkiLCJhIjoiY2tveGQ1OGs4MGV4bzJucGRoZ2VlNDVqbSJ9.Jv0nA83huBK0OEc6cmZaTg',
-  // });
+  const [viewport, setViewport] = React.useState({
+    longitude: -122.45,
+    latitude: 37.78,
+    zoom: 14,
+  });
 
-  // in render()
-const [viewport, setViewport] = React.useState({
-  longitude: -122.45,
-  latitude: 37.78,
-  zoom: 14,
-});
-return (
-  <ReactMapGL {...viewport} width="100vw" height="100vh" onViewportChange={setViewport}>
-    <Marker latitude={37.78} longitude={-122.41} offsetLeft={-20} offsetTop={-10}>
-      <div>You are here</div>
-    </Marker>
-  </ReactMapGL>
-);
+  const [farmInfo, setFarmInfo] = useState('');
+
+  const coordinates = [
+    {
+      longitude: -122.45,
+      latitude: 37.78,
+      name: 'Farm1',
+      description: 'We are farm1',
+    },
+    {
+      longitude: -8.7942,
+      latitude: -16.31928,
+      name: 'Farm2',
+      description: 'We are farm2',
+    },
+    {
+      longitude: -148.83206,
+      latitude: 11.95208,
+      name: 'Farm3',
+      description: 'We are farm3',
+    },
+  ];
+
+  const redMarker = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      fill="red"
+      className="bi bi-geo-alt-fill"
+      viewBox="0 0 16 16"
+    >
+      <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+    </svg>
+  );
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      width="50vw"
+      height="50vh"
+      onViewportChange={setViewport}
+      mapboxApiAccessToken="pk.eyJ1IjoiZmFybXkiLCJhIjoiY2tveGQ1OGs4MGV4bzJucGRoZ2VlNDVqbSJ9.Jv0nA83huBK0OEc6cmZaTg"
+    >
+      {coordinates.map(({ longitude, latitude, name, description }) => (
+        <Marker
+          latitude={latitude}
+          longitude={longitude}
+          offsetLeft={-20}
+          offsetTop={-10}
+          key={name}
+        >
+          <div
+            style={{ cursor: 'pointer' }}
+            onClick={() => setFarmInfo({ name, description, longitude, latitude })}
+          >
+            {redMarker}
+          </div>
+        </Marker>
+      ))}
+      {farmInfo && (
+        <Popup
+          tipSize={5}
+          anchor="top"
+          longitude={farmInfo.longitude}
+          latitude={farmInfo.latitude}
+          closeOnClick={false}
+          onClose={() => setFarmInfo('')}
+        >
+          <div>
+            <h3>{farmInfo.name}</h3>
+            <p>{farmInfo.description}</p>
+          </div>
+        </Popup>
+      )}
+    </ReactMapGL>
+  );
 };
 
 export default FarmsMap;
