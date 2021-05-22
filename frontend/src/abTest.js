87f6a23d5b434bfece970bb6ac5297eec2785854
fadi <fadi.alset.94@gmail.com>
2021-05-22 13:53:00 +0200

    Setup The AB/testing functions

---
+++ frontend/src/abTest.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Experiment, Variant } from 'react-optimize'
+import CartScreen from './screens/CartScreen'
+const abTest = () => {
+    return(
+      <Experiment id="m8MsFatpRo-YDvRdlEoGyw">
+        <Variant id="0">
+          
+        </Variant>
+        <Variant id="1">
+         
+        </Variant>
+      </Experiment>
+    )
+  }
+
+export default abTest
