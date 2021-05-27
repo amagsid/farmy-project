66ee32db6dbd966727d0c57c78d87846eae7a6f5
Khaled Alsaleh <khalsalehsyria1@gmail.com>
2021-05-27 20:48:26 +0200

    Insert More Data

--- backend/data/chefsTableIngredients.js
+++ backend/data/chefsTableIngredients.js
@@ -27,6 +27,24 @@ const chefsTableIngredients = [
     countInStock: 50,
   },
   {
+    name: 'Tomatoes',
+    image:
+      'https://images.unsplash.com/photo-1597160308671-bb9ec5348ae3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dG9tYXRvZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    description: 'Organic Tomatoes',
+    category: ['Vegetable', 'Vegetables', 'Healthy'],
+    price: 2.5,
+    countInStock: 50,
+  },
+  {
+    name: 'Red Onions',
+    image:
+      'https://images.unsplash.com/photo-1467019972079-a273e1bc9173?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVkJTIwb25pb25zfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    description: 'Organic Red Onions',
+    category: ['Vegetable', 'Vegetables', 'Healthy'],
+    price: 1.99,
+    countInStock: 50,
+  },
+  {
     name: 'Parsnip',
     image:
       'https://images.unsplash.com/photo-1606757389929-2045c644233b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGFyc25pcHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
@@ -60,6 +78,15 @@ const chefsTableIngredients = [
     description: 'Organic, free-range eggs',
     category: ['Free-range', 'Protein', 'Chicken'],
     price: 10.99,
+    countInStock: 50,
+  },
+  {
+    name: 'Parsley',
+    image:
+      'https://images.unsplash.com/photo-1590759485418-90509afec818?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGFyc2xleXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    description: 'Organic Parsley',
+    category: ['Greens', 'Spices', 'Herbs'],
+    price: 1.99,
     countInStock: 50,
   },
 ];
