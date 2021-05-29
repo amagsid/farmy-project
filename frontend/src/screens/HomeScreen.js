8e164e1b2be2a8b1a1beb10be031c1654df3e7d6
Ekmel Kalayci <ekmkal@gmail.com>
2021-05-29 14:41:23 +0200

    loading spin for whole homescreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -39,178 +39,186 @@ const HomeScreen = ({ match }) => {
   return (
     <>
       <Meta />
-      {loading && loadingLatest && <Loader />}
-      {error && errorLatest && <Message variant="danger">{error}</Message>}
-      {!keyword ? (
+      {/* {loading && loadingLatest && <Loader />}
+      {error && errorLatest && <Message variant="danger">{error}</Message>} */}
+      {loading && loadingLatest ? (
+        <Loader />
+      ) : error && errorLatest ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
         <>
-          {userInfo && <Message variant="success">Welcome {userInfo.name}!</Message>}
-
-          <Carousel style={{ color: 'white', fontSize: 'large' }}>
-            <Carousel.Item>
-              <img
-                className=" "
-                src="https://i.ibb.co/gmGbMtP/istock-506164764-pesticide-epandage-1920-web.jpg"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3 style={{ color: 'white' }}>Support your locals</h3>
-                <p className="label">
-                  A lot of products are produced locally. We want to make sure you eat healthy, and
-                  pay less.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="https://i.ibb.co/NZybdWC/davide-cantelli-jpkfc5-d-DI-unsplash.jpg"
-                alt="Second slide"
-              />
-              <Carousel.Caption>
-                <h3 style={{ color: 'white' }}>Healthy & hand-crafted</h3>
-                <p className="label">
-                  The healthy eating you deserve straight from your freezer. 100% taste. Zero faff
-                  necessary.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="https://i.ibb.co/jkNdx3H/Farmy.jpg"
-                alt="Delivery"
-              />
-              <Carousel.Caption>
-                <h3 style={{ color: 'white' }}>Next day delivery</h3>
-                <p className="label">
-                  Sustainable packaging keeps everything cool for up to 24 hours, order by 9pm.
-                  Flash frozen and delivered in a flash.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-
-          {userInfo && userInfo.preferences?.diet !== '' ? (
-            <PersonalizedRecommendations
-              preferences={userInfo.preferences && userInfo.preferences.diet}
-            />
-          ) : (
-            userInfo && (
-              <h2 style={{ color: '#808080	' }} className="border text-center">
-                Please fill in{' '}
-                <Link to="/preferences" style={{ color: '#808080	' }}>
-                  preferences
-                </Link>{' '}
-                to see recommendations
-              </h2>
-            )
-          )}
-          {!bundles.length && !bundlesListLatest.length && (
-            <Message variant="primary">Nothing found</Message>
-          )}
-
-          {userInfo && (
+          {!keyword ? (
             <>
+              {userInfo && <Message variant="success">Welcome {userInfo.name}!</Message>}
+
+              <Carousel style={{ color: 'white', fontSize: 'large' }}>
+                <Carousel.Item>
+                  <img
+                    className=" "
+                    src="https://i.ibb.co/gmGbMtP/istock-506164764-pesticide-epandage-1920-web.jpg"
+                    alt="First slide"
+                  />
+                  <Carousel.Caption>
+                    <h3 style={{ color: 'white' }}>Support your locals</h3>
+                    <p className="label">
+                      A lot of products are produced locally. We want to make sure you eat healthy,
+                      and pay less.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="https://i.ibb.co/NZybdWC/davide-cantelli-jpkfc5-d-DI-unsplash.jpg"
+                    alt="Second slide"
+                  />
+                  <Carousel.Caption>
+                    <h3 style={{ color: 'white' }}>Healthy & hand-crafted</h3>
+                    <p className="label">
+                      The healthy eating you deserve straight from your freezer. 100% taste. Zero
+                      faff necessary.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="https://i.ibb.co/jkNdx3H/Farmy.jpg"
+                    alt="Delivery"
+                  />
+                  <Carousel.Caption>
+                    <h3 style={{ color: 'white' }}>Next day delivery</h3>
+                    <p className="label">
+                      Sustainable packaging keeps everything cool for up to 24 hours, order by 9pm.
+                      Flash frozen and delivered in a flash.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
+
+              {userInfo && userInfo.preferences?.diet !== '' ? (
+                <PersonalizedRecommendations
+                  preferences={userInfo.preferences && userInfo.preferences.diet}
+                />
+              ) : (
+                userInfo && (
+                  <h2 style={{ color: '#808080	' }} className="border text-center">
+                    Please fill in{' '}
+                    <Link to="/preferences" style={{ color: '#808080	' }}>
+                      preferences
+                    </Link>{' '}
+                    to see recommendations
+                  </h2>
+                )
+              )}
+              {!bundles.length && !bundlesListLatest.length && (
+                <Message variant="primary">Nothing found</Message>
+              )}
+
+              {userInfo && (
+                <>
+                  <Container className="mb-5">
+                    <h1>Latest Bundles</h1>
+                    <Row>
+                      {bundlesListLatest.map((bundle) => (
+                        <Col key={bundle._id}>
+                          <Bundle bundle={bundle} />
+                        </Col>
+                      ))}
+                    </Row>
+                  </Container>
+                </>
+              )}
               <Container className="mb-5">
-                <h1>Latest Bundles</h1>
-                <Row>
-                  {bundlesListLatest.map((bundle) => (
-                    <Col key={bundle._id}>
-                      <Bundle bundle={bundle} />
-                    </Col>
-                  ))}
-                </Row>
+                <h1>Our Bundles</h1>
+
+                <Filter keyword={keyword} />
+                {loading && <Loader />}
+                {bundles.length === 0 && !loading ? (
+                  <h3>No Bundles Found!</h3>
+                ) : (
+                  <Row>
+                    {bundles &&
+                      bundles.length &&
+                      bundles.map((bundle) => (
+                        <Col sm={12} md={6} lg={6} xl={3} key={bundle._id}>
+                          <Link to={`/bundles/${bundle._id}`}>
+                            <Bundle bundle={bundle} />
+                          </Link>
+                          <LinkContainer to={`/bundles/${bundle._id}`}>
+                            <Button variant="outline-success" size="lg" block>
+                              Subscribe
+                            </Button>
+                          </LinkContainer>
+                        </Col>
+                      ))}
+                  </Row>
+                )}
+              </Container>
+
+              <Container className="mb-4">
+                <h1>Our Farmers</h1>
+                <FarmsMap />
+                <FarmStory />
               </Container>
             </>
-          )}
-          <Container className="mb-5">
-            <h1>Our Bundles</h1>
+          ) : (
+            <Container className="mb-5">
+              <Link to="/" className="btn btn-light">
+                Go Back
+              </Link>
+              {keyword && <h1>Search Results for "{keyword}"</h1>}
 
-            <Filter keyword={keyword} />
-            {loading && <Loader />}
-            {bundles.length === 0 && !loading ? (
-              <h3>No Bundles Found!</h3>
-            ) : (
-              <Row>
-                {bundles &&
-                  bundles.length &&
-                  bundles.map((bundle) => (
-                    <Col sm={12} md={6} lg={6} xl={3} key={bundle._id}>
+              {bundles.length ? (
+                <Row>
+                  {bundles.map((bundle) => (
+                    <Col key={bundle._id} md={4}>
                       <Link to={`/bundles/${bundle._id}`}>
                         <Bundle bundle={bundle} />
                       </Link>
-                      <LinkContainer to={`/bundles/${bundle._id}`}>
+                      <LinkContainer to={`/subscription/${bundle._id}`}>
                         <Button variant="outline-success" size="lg" block>
                           Subscribe
                         </Button>
                       </LinkContainer>
                     </Col>
                   ))}
-              </Row>
-            )}
-          </Container>
-
-          <Container className="mb-4">
-            <h1>Our Farmers</h1>
-            <FarmsMap />
-            <FarmStory />
+                </Row>
+              ) : (
+                <>
+                  <Message variant="danger">No results found.</Message>
+                  <h3>You may also like</h3>
+                  <Row>
+                    {bundlesListLatest.map((bundle) => (
+                      <Col key={bundle._id}>
+                        <Bundle bundle={bundle} />
+                        <LinkContainer to={`/bundles/${bundle._id}`}>
+                          <Button variant="outline-success" size="lg" block>
+                            Subscribe
+                          </Button>
+                        </LinkContainer>
+                      </Col>
+                    ))}
+                  </Row>
+                </>
+              )}
+            </Container>
+          )}
+          <Container className="mb-5">
+            <h1>Loved by thousands of customers</h1>
+            <Row>
+              {feedback &&
+                feedback.map((feed, index) => {
+                  return (
+                    <Col key={index}>
+                      <FeedBack text={feed.feedback} name={feed.name} />
+                    </Col>
+                  );
+                })}
+            </Row>
           </Container>
         </>
-      ) : (
-        <Container className="mb-5">
-          <Link to="/" className="btn btn-light">
-            Go Back
-          </Link>
-          {keyword && <h1>Search Results for "{keyword}"</h1>}
-
-          {bundles.length ? (
-            <Row>
-              {bundles.map((bundle) => (
-                <Col key={bundle._id} md={4}>
-                  <Link to={`/bundles/${bundle._id}`}>
-                    <Bundle bundle={bundle} />
-                  </Link>
-                  <LinkContainer to={`/subscription/${bundle._id}`}>
-                    <Button variant="outline-success" size="lg" block>
-                      Subscribe
-                    </Button>
-                  </LinkContainer>
-                </Col>
-              ))}
-            </Row>
-          ) : (
-            <>
-              <Message variant="danger">No results found.</Message>
-              <h3>You may also like</h3>
-              <Row>
-                {bundlesListLatest.map((bundle) => (
-                  <Col key={bundle._id}>
-                    <Bundle bundle={bundle} />
-                    <LinkContainer to={`/bundles/${bundle._id}`}>
-                      <Button variant="outline-success" size="lg" block>
-                        Subscribe
-                      </Button>
-                    </LinkContainer>
-                  </Col>
-                ))}
-              </Row>
-            </>
-          )}
-        </Container>
       )}
-      <Container className="mb-5">
-        <h1>Loved by thousands of customers</h1>
-        <Row>
-          {feedback &&
-            feedback.map((feed, index) => {
-              return (
-                <Col key={index}>
-                  <FeedBack text={feed.feedback} name={feed.name} />
-                </Col>
-              );
-            })}
-        </Row>
-      </Container>
     </>
   );
 };
